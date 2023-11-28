86803cf3e98b1486da50ca1a8f27e7707942f398
David Kudrot <davidkudrot@gmail.com>
2023-11-28 23:29:17 +0530

    This my class Module 10.5

---
+++ all_template_filters/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from datetime import date,time
+
+
+# Create your views here.
+
+def home(request):
+    context = {
+        'name' : 'MLA Fata Kestor',
+        'description' : 'Nithub Chakrabarty',
+        'dialogue' : 'Sala Marbo Ekhane Lash Poribi Sosane',
+        'tips' : 'Sala Code KOR r nahole PAGLAMO char',
+        'array' : ['ek ake ek', 'ek dugune dui', 'tin ake tin', 'char ake char', 'pach ake pach', 'choi ake choi', 'sat ake sar', 'att ake att', 'noi ake noi', 'dos ake dos', 'chup chap bosh'],
+        'numbers' : [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115],
+        'date' : date(2023, 11, 28) 
+    }
+    return render(request, 'home.html', {'data' : context})
